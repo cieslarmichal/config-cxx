c40677da11f0e4e90a132f401f5d0b935268ae8c
Michal Cieslar <michalovskyyy@gmail.com>
2023-12-22 17:52:35 +0100

    drop mingw support

--- src/tests/EnvironmentSetter.cpp
+++ src/tests/EnvironmentSetter.cpp
@@ -6,7 +6,7 @@ namespace config::tests
 {
 void EnvironmentSetter::setEnvironmentVariable(const std::string& envName, const std::string& envValue)
 {
-#if defined(_WIN32) || defined(__MINGW32__) || defined(__MINGW64__)
+#if defined(_WIN32)
     _putenv_s(envName.c_str(), envValue.c_str());
 #else
     setenv(envName.c_str(), envValue.c_str(), 1);
