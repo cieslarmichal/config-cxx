d261703ebcb92659dc3bb4cc8b3141387389dc8a
Bodhi2011 <101071809+Bodhi2011@users.noreply.github.com>
2024-02-02 13:24:49 -0500

    got json working

---
+++ src/JsonConfigLoaderTest.cpp
@@ -0,0 +1,90 @@
+#include "JsonConfigLoader.h"
+#include "config-cxx/Config.h"
+
+#include <filesystem>
+#include <fstream>
+#include <unordered_map>
+#include <any>
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+#include "filesystem/ExecutableFinder.h"
+#include "tests/EnvironmentSetter.h"
+
+using namespace ::testing;
+using namespace config;
+using namespace config::tests;
+using namespace config::filesystem;
+
+namespace
+{
+const auto projectRootPath = ExecutableFinder::getExecutablePath();
+const auto testConfigDirectory = projectRootPath.parent_path() / "testConfig";
+const auto testEnvConfigFilePath = testConfigDirectory / "test.json";
+
+const std::string testJson = R"(
+{
+    "db": {
+        "port": 1996
+    },
+    "aws": {
+        "accountId": "123456789012",
+        "accountKey": "123",
+        "region": "eu-central-1"
+    },
+    "auth": {
+        "expiresIn": 3600,
+        "enabled": true,
+        "roles": [
+            "admin",
+            "user"
+        ]
+    }
+}
+)";
+}
+
+class JsonConfigLoaderTest : public Test
+{
+    public:
+    void SetUp() override
+    {
+        EnvironmentSetter::setEnvironmentVariable("CXX_ENV", "test");
+        EnvironmentSetter::setEnvironmentVariable("CXX_CONFIG_DIR", testConfigDirectory.string());
+
+        std::filesystem::remove_all(testConfigDirectory);
+
+        std::filesystem::create_directory(testConfigDirectory);
+
+        std::ofstream testEnvConfigFile{testEnvConfigFilePath};
+
+        testEnvConfigFile << testJson;
+    }
+
+    void TearDown() override
+    {
+        EnvironmentSetter::setEnvironmentVariable("CXX_ENV", "test");
+        EnvironmentSetter::setEnvironmentVariable("CXX_CONFIG_DIR", testConfigDirectory.string());
+
+        std::filesystem::remove_all(testConfigDirectory);
+    }
+};
+
+TEST_F(JsonConfigLoaderTest, loadConfigFile) {
+    std::unordered_map<std::string, std::any> expectedValues = {
+        {"db.port", 1996},
+        {"aws.accountId", "123456789012"},
+        {"aws.accountKey", "123"},
+        {"aws.region", "eu-central-1"},
+        {"auth.expiresIn", 3600},
+        {"auth.enabled", true},
+        {"auth.roles.0", "admin"},
+        {"auth.roles.1", "user"}
+    };
+
+    std::unordered_map<std::string, std::any> configValues;
+    JsonConfigLoader::loadConfigFile(testEnvConfigFilePath, configValues);
+}
